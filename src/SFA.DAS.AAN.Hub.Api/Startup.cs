2c00719f9e2b5bda43273ae692b93acfb65b638b
Liam Wright <mail@liam-wright.co.uk>
2022-10-26 13:55:03 +0100

    CSP-340 - Split Program.cs to Startup.cs as per AS standards

---
+++ src/SFA.DAS.AAN.Hub.Api/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace SFA.DAS.AAN.Hub.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            var config = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables();
+
+#if DEBUG
+            config.AddJsonFile($"appsettings.Development.json", optional: true);
+#endif
+
+            Configuration = config.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationInsightsTelemetry();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SFA.DAS.AAN.Hub.Api", Version = "v1" });
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SFA.DAS.AAN.Hub.Api v1"));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+        }
+    }
+}
