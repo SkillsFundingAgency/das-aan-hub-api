893c95992dc73f39ffe1d11d823e07cf26ffcb0c
Liam Wright <mail@liam-wright.co.uk>
2022-10-31 14:49:42 +0000

    CSP-351 - Calendars, Events and Attendance DB tables

---
+++ src/SFA.DAS.AAN.Hub.Database/Tables/Calendar.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Calendar]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1, 1) PRIMARY KEY, 
+    [CalendarName] NVARCHAR(200) NOT NULL, 
+    [Description] NVARCHAR(200) NOT NULL, 
+    [EffectiveFrom] DATE NOT NULL, 
+    [EffectiveTo] DATE NOT NULL, 
+    [IsActive] BIT NOT NULL
+)
