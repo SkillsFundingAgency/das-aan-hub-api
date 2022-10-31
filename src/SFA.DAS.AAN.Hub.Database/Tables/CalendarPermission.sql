32d73bebdc8c10639ca3afcf6a0317d7857d0eb1
Liam Wright <mail@liam-wright.co.uk>
2022-10-31 14:49:42 +0000

    CSP-351 - Calendars, Events and Attendance DB tables

---
+++ src/SFA.DAS.AAN.Hub.Database/Tables/CalendarPermission.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[CalendarPermission]
+(
+	[CalendarId] BIGINT NOT NULL PRIMARY KEY, 
+    [PermissionId] BIGINT NOT NULL, 
+    [Create] BIT NOT NULL, 
+    [Update] BIT NOT NULL, 
+    [View] BIT NOT NULL DEFAULT 1, 
+    [Delete] BIT NOT NULL
+)
