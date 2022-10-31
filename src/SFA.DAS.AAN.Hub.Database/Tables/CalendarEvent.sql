a26b5ac754164cbfc8f388df93ee76314df8e8f0
Liam Wright <mail@liam-wright.co.uk>
2022-10-31 14:49:42 +0000

    CSP-351 - Calendars, Events and Attendance DB tables

---
+++ src/SFA.DAS.AAN.Hub.Database/Tables/CalendarEvent.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[CalendarEvent]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1, 1) PRIMARY KEY, 
+    [CalendarId] BIGINT NOT NULL, 
+    [CreatedByUserId] BIGINT NOT NULL, 
+    [Start] DATETIME NOT NULL, 
+    [End] DATETIME NOT NULL, 
+    [Description] NVARCHAR(200) NOT NULL, 
+    [Location] NVARCHAR(200) NULL, 
+    [Postcode] NVARCHAR(10) NULL, 
+    [EventLink] NCHAR(200) NULL, 
+    [Contact] NCHAR(200) NOT NULL, 
+    [ContactEmail] NCHAR(256) NOT NULL, 
+    [UpdatedByUserId] BIGINT NOT NULL, 
+    [Created] DATETIME NOT NULL DEFAULT GETUTCDATE(), 
+    [Updated] DATETIME NULL, 
+    [Deleted] DATETIME NULL, 
+    [IsActive] BIT NOT NULL
+)
