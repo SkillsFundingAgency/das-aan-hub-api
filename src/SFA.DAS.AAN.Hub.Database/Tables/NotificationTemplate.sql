70fe2cd1c872fa5b51b500bfe5f0f7a2ed294daa
Liam Wright <mail@liam-wright.co.uk>
2022-10-31 15:33:50 +0000

    CSP-352 - Notifications DB tables

---
+++ src/SFA.DAS.AAN.Hub.Database/Tables/NotificationTemplate.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[NotificationTemplate]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1, 1) PRIMARY KEY, 
+    [Type] NVARCHAR(10) NOT NULL, 
+    [Description] NVARCHAR(200) NOT NULL, 
+    [MessageBody] NVARCHAR(256) NOT NULL, 
+    [TemplateId] BIGINT NOT NULL, 
+    [MergeFields] NVARCHAR(MAX) NOT NULL, 
+    [IsActive] BIT NOT NULL, 
+    [IsSystem] BIT NOT NULL
+)
