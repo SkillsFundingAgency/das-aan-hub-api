c6304895e7a25e2343229c5273eae0a4fa22244d
Liam Wright <mail@liam-wright.co.uk>
2022-11-02 16:29:29 +0000

    CSP-363 - /Region endpoint

---
+++ src/SFA.DAS.AAN.Application.UnitTests/Queries/WhenRequestingGetAllRegions.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using SFA.DAS.AAN.Application.Queries.GetAllRegions;
+using SFA.DAS.AAN.Data;
+using SFA.DAS.AAN.Domain.Entities;
+
+namespace SFA.DAS.AAN.Application.UnitTests.Queries
+{
+    public class WhenRequestingGetAllRegions
+    {
+        [Theory, AutoMoqData]
+        public async Task ThenAllRegionsAreReturned(
+            GetAllRegionsQuery query,
+            [Frozen(Matching.ImplementedInterfaces)] AanDataContext context,
+            GetAllRegionsQueryHandler handler,
+            List<Region> response)
+        {
+
+            context.Regions.AddRange(response);
+            context.SaveChanges();
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result?.Regions?.Count.Should().Be(response.Count);
+        }
+    }
+}
---
+++ src/SFA.DAS.AAN.Domain/Entities/Region.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.AAN.Domain.Entities
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string Area { get; set; }
+        public int Ordering { get; set; }
+    }
+}
