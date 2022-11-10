7890005f8f0216303aae7770dea2a68f443183ec
Liam Wright <mail@liam-wright.co.uk>
2022-11-10 12:35:06 +0000

    CSP-351 - Added 1-to-many foreign keys, and updated MemberId types

--- src/SFA.DAS.AAN.Hub.Database/Tables/Notifications.sql
+++ src/SFA.DAS.AAN.Hub.Database/Tables/Notifications.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Notifications]
 (
 	[Id] BIGINT NOT NULL IDENTITY(1, 1) PRIMARY KEY, 
-    [MemberId] BIGINT NOT NULL, 
+    [MemberId] UNIQUEIDENTIFIER NOT NULL, 
     [NotificationTemplateId] BIGINT NOT NULL, 
     [MergeValues] NVARCHAR(MAX) NOT NULL, 
     [CreatedBy] BIGINT NULL, 
@@ -9,4 +9,6 @@
     [SentTime] DATETIME NOT NULL, 
     [Viewed] BIT NOT NULL, 
     [ViewedTime] DATETIME NOT NULL
+    CONSTRAINT [FK_Notifications_Member] FOREIGN KEY ([MemberId]) REFERENCES [Member]([Id])
+    CONSTRAINT [FK_Notifications_NotificationTemplate] FOREIGN KEY ([NotificationTemplateId]) REFERENCES [NotificationTemplate]([Id])
 )
