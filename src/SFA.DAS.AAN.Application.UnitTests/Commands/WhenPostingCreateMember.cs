74a4a3b6a2393ec88495172b471d1ddf62f2a531
Ian Rose <ian.rose@digital.education.gov.uk>
2022-11-21 15:19:16 +0000

    CSP-365 Add unit tests

---
+++ src/SFA.DAS.AAN.Application.UnitTests/Commands/WhenPostingCreateMember.cs
@@ -0,0 +1,91 @@
+﻿
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SFA.DAS.AAN.Application.Commands.CreateMember;
+using SFA.DAS.AAN.Hub.Api.Controllers;
+using SFA.DAS.AAN.Application.ApiResponses;
+using SFA.DAS.AAN.Application.UnitTests;
+using SFA.DAS.AAN.Domain.Enums;
+
+
+namespace SFA.DAS.AAN.Hub.Api.UnitTests.Members
+{
+    public class WhenPostingCreateMember
+    {
+        private readonly Mock<IMediator> _mediator;
+        private readonly MemberController _controller;
+
+        public WhenPostingCreateMember()
+        {
+            _mediator = new Mock<IMediator>();
+            _controller = new MemberController(_mediator.Object, Mock.Of<ILogger<MemberController>>());
+        }
+
+        [Theory, AutoMoqData]
+        public async Task And_MediatorApprenticeCommandSuccessful_Then_ReturnOk(
+            CreateMemberCommand command,
+            CreateMemberResponse response
+            )
+        {
+            IActionResult result = await ExecuteMediatorCommand(command, response, MembershipUserTypes.Apprentice);
+            await ApplyTests(result, response);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task And_MediatorEmployerCommandSuccessful_Then_ReturnOk(
+            CreateMemberCommand command,
+            CreateMemberResponse response
+            )
+        {
+            IActionResult result = await ExecuteMediatorCommand(command, response, MembershipUserTypes.Employer);
+            await ApplyTests(result, response);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task And_MediatorPartnerCommandSuccessful_Then_ReturnOk(
+            CreateMemberCommand command,
+            CreateMemberResponse response
+            )
+        {
+            IActionResult result = await ExecuteMediatorCommand(command, response, MembershipUserTypes.Partner);
+            await ApplyTests(result, response);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task And_MediatorAdminCommandSuccessful_Then_ReturnOk(
+            CreateMemberCommand command,
+            CreateMemberResponse response
+            )
+        {
+            IActionResult result = await ExecuteMediatorCommand(command, response, MembershipUserTypes.Admin);
+            await ApplyTests(result, response);
+        }
+
+
+
+
+        private async Task<IActionResult> ExecuteMediatorCommand(
+            CreateMemberCommand command,
+            CreateMemberResponse response,
+            MembershipUserTypes userType
+            )
+        {
+            command.UserType = userType;
+            _mediator.Setup(m => m.Send(command, It.IsAny<CancellationToken>())).ReturnsAsync(response);
+            return await _controller.CreateAdminMember(command);
+        }
+
+        private async Task ApplyTests(IActionResult result, CreateMemberResponse response)
+        {
+            result.Should().NotBeNull();
+
+            object? model = ((OkObjectResult)result).Value;
+            model.Should().NotBeNull();
+            model.Should().BeAssignableTo<CreateMemberApiResponse>();
+            model.Should().BeEquivalentTo(new CreateMemberApiResponse(response));
+        }
+    }
+}
