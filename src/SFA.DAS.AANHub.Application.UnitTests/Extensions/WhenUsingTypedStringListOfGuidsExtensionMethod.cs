41168205b0dd971dd760869d0b6d35412702aa3c
Ian Rose <ian.rose@digital.education.gov.uk>
2022-11-30 15:15:26 +0000

    Unit tests

---
+++ src/SFA.DAS.AANHub.Application.UnitTests/Extensions/WhenUsingTypedStringListOfGuidsExtensionMethod.cs
@@ -0,0 +1,56 @@
+﻿
+using FluentAssertions;
+using SFA.DAS.AANHub.Application.Extensions;
+
+
+namespace SFA.DAS.AANHub.Api.UnitTests.Application.Extensions
+{
+    public class WhenUsingTypedStringListOfGuidsExtensionMethod
+    {
+        private const string str1 = "00000000-0000-0000-0000-000000000001";
+        private const string str2 = "00000000-0000-0000-0000-000000000002";
+        private const string str3 = "00000000-0000-0000-0000-000000000003";
+        private const string str4 = "00000000-0000-0000-0000-000000000004";
+        private readonly Guid guid1 = new Guid(str1);
+        private readonly Guid guid2 = new Guid(str2);
+        private readonly Guid guid3 = new Guid(str3);
+        private readonly Guid guid4 = new Guid(str4);
+
+        [Fact]
+        public void And_PassingValidStringGuidList_Then_ReturnEnumerable()
+        {
+            var input = $"{str1},{str2},{str3}";
+            var expected = new List<Guid>(new Guid[] { guid1, guid2, guid3 });
+            var output = input.ToGuidList(",");
+
+            output.Should().BeAssignableTo<IEnumerable<Guid>>();
+            output.Should().NotBeNullOrEmpty();
+            output.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void And_PassingEmptyStringGuidList_Then_ReturnEmptyEnumerable()
+        {
+            var input = "";
+            var expected = Enumerable.Empty<Guid>();
+            var output = input.ToGuidList(",");
+
+            output.Should().BeAssignableTo<IEnumerable<Guid>>();
+            output.Should().NotBeNull();
+            output.Should().BeEmpty();
+            output.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void And_PassingStringGuidListWithInvalidElements_Then_ReturnEnumerableWithValidElements()
+        {
+            var input = $"{str1},{str2},123,{str3},{str4},4x6,789";
+            var expected = new List<Guid>(new Guid[] { guid1, guid2, guid3, guid4 });
+            var output = input.ToGuidList(",");
+
+            output.Should().BeAssignableTo<IEnumerable<Guid>>();
+            output.Should().NotBeNullOrEmpty();
+            output.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ src/SFA.DAS.AANHub.Application.UnitTests/Extensions/WhenUsingTypedStringListOfIntsExtensionMethod.cs
@@ -0,0 +1,47 @@
+﻿
+using FluentAssertions;
+using SFA.DAS.AANHub.Application.Extensions;
+
+
+namespace SFA.DAS.AANHub.Api.UnitTests.Application.Extensions
+{
+    public class WhenUsingTypedStringListOfIntsExtensionMethod
+    {
+        [Fact]
+        public void And_PassingValidStringIntList_Then_ReturnEnumerable()
+        {
+            var input = "1,2,3";
+            var expected = new List<Int64>(new Int64[] { 1, 2, 3 });
+            var output = input.ToIntList(",");
+
+            output.Should().BeAssignableTo<IEnumerable<Int64>>();
+            output.Should().NotBeNullOrEmpty();
+            output.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void And_PassingEmptyStringIntList_Then_ReturnEmptyEnumerable()
+        {
+            var input = "";
+            var expected = Enumerable.Empty<Int64>();
+            var output = input.ToIntList(",");
+
+            output.Should().BeAssignableTo<IEnumerable<Int64>>();
+            output.Should().NotBeNull();
+            output.Should().BeEmpty();
+            output.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void And_PassingStringIntListWithInvalidElements_Then_ReturnEnumerableWithValidElements()
+        {
+            var input = "1,2,a,3,4,b,c";
+            var expected = new List<Int64>(new Int64[] { 1, 2, 3, 4 });
+            var output = input.ToIntList(",");
+
+            output.Should().BeAssignableTo<IEnumerable<Int64>>();
+            output.Should().NotBeNullOrEmpty();
+            output.Should().BeEquivalentTo(expected);
+        }
+    }
+}
