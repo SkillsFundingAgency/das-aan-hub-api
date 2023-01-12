b6262de732c7cd12f70753958053c4f7fc92a009
Liam Wright <mail@liam-wright.co.uk>
2023-01-12 11:27:12 +0000

    CSP-485 - Updated /Regions endpoint to current standards

--- src/SFA.DAS.AANHub.Application/Queries/GetAllRegions/GetAllRegionsQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.AANHub.Application.Queries.GetAllRegions
-{
-    public class GetAllRegionsQuery : IRequest<GetAllRegionsResult>
-    {
-    }
-}
---
+++ src/SFA.DAS.AANHub.Application/Regions/Queries/GetAllRegionsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace SFA.DAS.AANHub.Application.Regions.Queries
+{
+    public class GetAllRegionsQuery : IRequest<GetAllRegionsQueryResult>
+    {
+    }
+}
