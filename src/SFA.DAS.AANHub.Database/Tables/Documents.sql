3f5c312dc80f45db9bafb8f1b5a0fcdbac277613
Liam Wright <mail@liam-wright.co.uk>
2023-02-06 11:58:31 +0000

    CSP-480 - Removed Created from dbo.Documents

--- src/SFA.DAS.AANHub.Database/Tables/Documents.sql
+++ src/SFA.DAS.AANHub.Database/Tables/Documents.sql
@@ -2,7 +2,6 @@
 (
     [Id] BIGINT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
     [MemberId] UNIQUEIDENTIFIER NOT NULL, 
-    [Created] DATETIME NOT NULL, 
     [Title] NVARCHAR(200) NOT NULL, 
     [FileName] NCHAR(200) NOT NULL, 
     [Keywords] NVARCHAR(MAX) NOT NULL, 
