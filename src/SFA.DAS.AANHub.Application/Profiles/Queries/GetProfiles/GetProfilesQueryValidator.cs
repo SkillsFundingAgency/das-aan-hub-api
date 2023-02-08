6a1860d6d04f7a7f9383a931289110c599e5fdd3
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-02-08 13:58:41 +0000

    CSP-554-Updated GetProfiles end point

---
+++ src/SFA.DAS.AANHub.Application/Profiles/Queries/GetProfiles/GetProfilesQueryValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace SFA.DAS.AANHub.Application.Profiles.Queries.GetProfiles
+{
+    public class GetProfilesQueryValidator : AbstractValidator<GetProfilesQuery>
+    {
+    }
+}
--- src/SFA.DAS.AANHub.Data/Extensions/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.AANHub.Data/Extensions/ServiceCollectionExtensions.cs
@@ -49,6 +49,7 @@ namespace SFA.DAS.AANHub.Data.Extensions
             services.AddTransient<IMembersWriteRepository, MembersWriteRepository>();
             services.AddTransient<IMembersReadRepository, MembersReadRepository>();
             services.AddTransient<IMembersPermissionsReadRepository, MembersPermissionsReadRepository>();
+            services.AddTransient<IProfilesReadRepository, ProfilesReadRepository>();
         }
 
         public static async Task<string> GenerateTokenAsync()
