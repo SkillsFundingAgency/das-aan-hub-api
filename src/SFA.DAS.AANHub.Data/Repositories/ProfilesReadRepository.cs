d67c24667baeb3893f37bb7fbca63adfeb831b32
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-02-10 13:43:52 +0000

    CSP-554-Resolved codesmell

--- src/SFA.DAS.AANHub.Data/Repositories/ProfilesReadRepository.cs
+++ src/SFA.DAS.AANHub.Data/Repositories/ProfilesReadRepository.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using SFA.DAS.AANHub.Domain.Entities;
 using SFA.DAS.AANHub.Domain.Interfaces.Repositories;
 
 namespace SFA.DAS.AANHub.Data.Repositories
 {
+    [ExcludeFromCodeCoverage]
     internal class ProfilesReadRepository : IProfilesReadRepository
     {
         private readonly AanDataContext _aanDataContext;
