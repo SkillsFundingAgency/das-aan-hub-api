d2833e000c196e7b44fd9cfa817a42d8fe6ae3ec
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-02-14 17:04:04 +0000

    CSP-570-Add_ReasontojoinAmbassadorNetwork_AsProfileOption

--- src/SFA.DAS.AANHub.Database/Scripts/AddProfileData.sql
+++ src/SFA.DAS.AANHub.Database/Scripts/AddProfileData.sql
@@ -21,6 +21,7 @@ INSERT INTO #TempProfile VALUES
 (14,'Promoting the network on social media channels','Promotions',5, 'Apprentice'),
 (20,'Job title','Personal',1, 'Apprentice'),
 (21,'Engaged with a previous ambassador in the network','Personal',2, 'Apprentice'),
+(22,'Reason to join ambassador network','Personal',3, 'Apprentice'),
 (30,'Employer name','Employer',1, 'Apprentice'),
 (31,'Employer Address line 1','Employer',2, 'Apprentice'),
 (32,'Employer Address line 2','Employer',3, 'Apprentice'),
