f9b7532918a948035a70a2909d7d2beee027c40e
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-04-03 09:11:38 +0100

    CSP-683-AddAANApprenticeshipTableToAANHubDb - Address Code Review issue: improved on indentation

--- src/SFA.DAS.AANHub.Database/Tables/AANApprenticeship.sql
+++ src/SFA.DAS.AANHub.Database/Tables/AANApprenticeship.sql
@@ -1,12 +1,11 @@
-﻿CREATE TABLE [dbo].[AANApprenticeship]
-(
-    [Id] INT IDENTITY(1,1) NOT NULL,
-    [FirstName] [nvarchar](200) NOT NULL, 
-    [LastName] [nvarchar](200) NOT NULL,
-    [Email] [nvarchar](256) NOT NULL,
-    [DateOfBirth] [datetime2](7) NOT NULL,
-    [Uln] [bigint] NULL,
-    [ApprenticeshipId] [bigint] NULL,
+﻿CREATE TABLE [dbo].[AANApprenticeship](
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](200) NOT NULL,
+	[LastName] [nvarchar](200) NOT NULL,
+	[Email] [nvarchar](256) NOT NULL,
+	[DateOfBirth] [datetime2](7) NOT NULL,
+	[Uln] [bigint] NULL,
+	[ApprenticeshipId] [bigint] NULL,
 	[StartDate] [datetime2](7) NOT NULL,
 	[EndDate] [datetime2](7) NULL,
 	[TrainingProviderId] [bigint] NULL,
@@ -15,6 +14,6 @@
 	[TrainingCourseOption] [nvarchar](126) NULL,
 	[StandardUId] [nvarchar](20) NULL,
 	[CreatedOn] [datetime2](7) NOT NULL DEFAULT (getutcdate()),
- 	CONSTRAINT PK_AANApprenticeship_Id PRIMARY KEY (Id),
+	CONSTRAINT PK_AANApprenticeship_Id PRIMARY KEY (Id),
 	CONSTRAINT UK_AANApprenticeship_Email_Lastname_DateOfBirth UNIQUE ([Email], [Lastname], [DateOfBirth])
 )
