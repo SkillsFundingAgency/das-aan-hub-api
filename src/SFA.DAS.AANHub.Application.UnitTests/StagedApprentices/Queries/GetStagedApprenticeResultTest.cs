054e3c86badca51e7d6c6facf970a69a07ac55e9
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-04-03 16:27:27 +0100

    CSP-684-AddGetApprenticeshipToAANHubAPI - Improve Coverage

---
+++ src/SFA.DAS.AANHub.Application.UnitTests/StagedApprentices/Queries/GetStagedApprenticeResultTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using SFA.DAS.AANHub.Application.StagedApprentices.Queries;
+using SFA.DAS.AANHub.Domain.Entities;
+
+namespace SFA.DAS.AANHub.Application.UnitTests.StagedApprentices.Queries
+{
+    [TestFixture]
+    public class GetStagedApprenticeResultTest
+    {
+        [Test, AutoMoqData]
+        public void Apprentice_ReturnsExpectedFields(StagedApprentice stagedApprentice)
+        {
+            var response = (GetStagedApprenticeResult?)stagedApprentice;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(response, Is.Not.Null);
+
+                Assert.AreEqual(stagedApprentice.Uln, response!.Uln);
+                Assert.AreEqual(stagedApprentice.ApprenticeshipId, response.ApprenticeshipId);
+                Assert.AreEqual(stagedApprentice.EmployerName, response.EmployerName);
+                Assert.AreEqual(stagedApprentice.StartDate, response.StartDate);
+                Assert.AreEqual(stagedApprentice.EndDate, response.EndDate);
+                Assert.AreEqual(stagedApprentice.TrainingProviderId, response.TrainingProviderId);
+                Assert.AreEqual(stagedApprentice.TrainingProviderName, response.TrainingProviderName);
+                Assert.AreEqual(stagedApprentice.TrainingCode, response.TrainingCode);
+                Assert.AreEqual(stagedApprentice.StandardUId, response.StandardUId);
+            });
+        }
+
+        [Test, AutoMoqData]
+        public void StagedApprentice_GetStagedApprenticeResultTest_StagedApprenticeIsNull()
+        {
+            StagedApprentice? stagedApprentice = null;
+            var response = (GetStagedApprenticeResult?)stagedApprentice!;
+            Assert.That(response, Is.Null);
+        }
+    }
+}
