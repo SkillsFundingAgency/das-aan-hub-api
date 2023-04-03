c2f9671b2f004e2fa8299ff6908fd85b60cee74e
Ravi Hejmadi <46343852+ravihejmadi@users.noreply.github.com>
2023-04-03 07:32:26 +0100

    CSP-683-AddAANApprenticeshipTableToAANHubDb - Initial Commit

---
+++ src/SFA.DAS.AANHub.Database/Tables/AANApprenticeship.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[AANApprenticeship]
+(
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [FirstName] [nvarchar](200) NOT NULL, 
+    [LastName] [nvarchar](200) NOT NULL,
+    [Email] [nvarchar](256) NOT NULL,
+    [DateOfBirth] [datetime2](7) NOT NULL,
+    [Uln] [bigint] NULL,
+    [ApprenticeshipId] [bigint] NULL,
+	[StartDate] [datetime2](7) NOT NULL,
+	[EndDate] [datetime2](7) NULL,
+	[TrainingProviderId] [bigint] NULL,
+	[TrainingProviderName] [nvarchar](200) NULL,
+	[TrainingCode] [nvarchar](15) NOT NULL,
+	[TrainingCourseOption] [nvarchar](126) NULL,
+	[StandardUId] [nvarchar](20) NULL,
+	[CreatedOn] [datetime2](7) NOT NULL DEFAULT (getutcdate()),
+ 	CONSTRAINT PK_AANApprenticeship_Id PRIMARY KEY (Id),
+	CONSTRAINT UK_AANApprenticeship_Email_Lastname_DateOfBirth UNIQUE ([Email], [Lastname], [DateOfBirth])
+)
