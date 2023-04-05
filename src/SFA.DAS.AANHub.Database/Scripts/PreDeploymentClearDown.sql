83a076e952640cd7a29be3b372bfc5b23f02eaa5
Deven Shah <devenshah1974@gmail.com>
2023-04-05 12:03:06 +0100

    CSP-701 Added PreDeploymentClearDown script

---
+++ src/SFA.DAS.AANHub.Database/Scripts/PreDeploymentClearDown.sql
@@ -0,0 +1,17 @@
+﻿IF EXISTS(SELECT * FROM sys.tables WHERE Name = 'AttendanceProfile')
+  DROP TABLE AttendanceProfile;
+GO
+
+IF EXISTS(SELECT * FROM sys.tables WHERE Name = 'CalendarPermission')
+  DROP TABLE CalendarPermission;
+GO
+
+IF EXISTS(SELECT * FROM sys.tables WHERE Name = 'MemberPermission')
+  DROP TABLE MemberPermission;
+GO
+
+IF EXISTS(SELECT * FROM sys.tables WHERE Name = 'Permission')
+  DROP TABLE Permission;
+GO
+
+
