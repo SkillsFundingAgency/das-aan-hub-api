648dccf303d602bb7ce105f46777da7f316eba7c
Deven Shah <devenshah1974@gmail.com>
2023-04-18 22:37:31 +0100

    CSP-501 Dropped MemberRegion table and added RegionId to Member

--- src/SFA.DAS.AANHub.Database/Tables/Member.sql
+++ src/SFA.DAS.AANHub.Database/Tables/Member.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[Member]
 (
-    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    [Id] UNIQUEIDENTIFIER NOT NULL,
     [UserType] NVARCHAR(10) NOT NULL,
     [Status] NVARCHAR(10) NOT NULL,
     [Information] NVARCHAR(MAX) NULL,
     [Joined] DATETIME NOT NULL,
+    [RegionId] INT NULL
+    CONSTRAINT [PK_Member] PRIMARY KEY ([Id])
+    CONSTRAINT [FK_Member_Region] FOREIGN KEY ([RegionId]) REFERENCES [Region] ([Id])
 )
--- src/SFA.DAS.AANHub.Database/Tables/MemberRegion.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[MemberRegion]
-(
-	[MemberId] UNIQUEIDENTIFIER NOT NULL, 
-    [RegionId] INT NOT NULL
-	CONSTRAINT [PK_MemberRegion] PRIMARY KEY (MemberId, RegionId)
-	CONSTRAINT [FK_MemberRegion_Member] FOREIGN KEY ([MemberId]) REFERENCES [Member]([Id])
-	CONSTRAINT [FK_MemberRegion_Region] FOREIGN KEY ([RegionId]) REFERENCES [Region]([Id])
-)
