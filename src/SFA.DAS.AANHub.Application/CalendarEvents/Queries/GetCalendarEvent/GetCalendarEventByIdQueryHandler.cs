d57fa52cf7b36cfb9d702de1a448ce976cc693b0
Saw Naw <saw.naw@digital.education.gov.uk>
2023-05-02 16:28:59 +0100

    CSP-764 Add working GET endpoint for CalendarEvent

---
+++ src/SFA.DAS.AANHub.Application/CalendarEvents/Queries/GetCalendarEvent/GetCalendarEventByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SFA.DAS.AANHub.Application.Mediatr.Responses;
+using SFA.DAS.AANHub.Domain.Interfaces.Repositories;
+
+namespace SFA.DAS.AANHub.Application.CalendarEvents.Queries.GetCalendarEvent;
+
+public class GetCalendarEventByIdQueryHandler : IRequestHandler<GetCalendarEventByIdQuery, ValidatedResponse<GetCalendarEventByIdQueryResult>>
+{
+    private readonly ICalendarEventsReadRepository _calendarEventsReadRepository;
+
+    public GetCalendarEventByIdQueryHandler(ICalendarEventsReadRepository calendarEventsReadRepository) => _calendarEventsReadRepository = calendarEventsReadRepository;
+
+    public async Task<ValidatedResponse<GetCalendarEventByIdQueryResult>> Handle(GetCalendarEventByIdQuery request, CancellationToken cancellationToken)
+    {
+        var calendarEvent = await _calendarEventsReadRepository.GetCalendarEvent(request.CalendarEventId);
+
+        // TODO: How to handle null?
+        return new ValidatedResponse<GetCalendarEventByIdQueryResult>(calendarEvent);
+    }
+}
