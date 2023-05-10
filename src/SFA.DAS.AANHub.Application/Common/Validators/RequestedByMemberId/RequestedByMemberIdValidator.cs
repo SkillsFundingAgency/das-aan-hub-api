4dd5a18034e4bc6e9b1ad60039308e88b332d616
Saw Naw <saw.naw@digital.education.gov.uk>
2023-05-10 09:55:30 +0100

    CSP-764 Rename one instance of "user" to "member"

--- src/SFA.DAS.AANHub.Application/Common/Validators/RequestedByMemberId/RequestedByMemberIdValidator.cs
+++ src/SFA.DAS.AANHub.Application/Common/Validators/RequestedByMemberId/RequestedByMemberIdValidator.cs
@@ -7,7 +7,7 @@ namespace SFA.DAS.AANHub.Application.Common.Validators.RequestedByMemberId
     public class RequestedByMemberIdValidator : AbstractValidator<IRequestedByMemberId>
     {
         private const string RequestedByMemberHeaderEmptyErrorMessage = "X-RequestedByMemberId header is empty";
-        private const string RequestedByMemberIdNotFoundMessage = "Could not find a valid active user ID matching the X-RequestedByMemberId header";
+        private const string RequestedByMemberIdNotFoundMessage = "Could not find a valid active Member ID matching the X-RequestedByMemberId header";
         public RequestedByMemberIdValidator(IMembersReadRepository membersReadRepository) => RuleFor(x => x.RequestedByMemberId)
                 .Cascade(CascadeMode.Stop)
                 .NotEmpty()
