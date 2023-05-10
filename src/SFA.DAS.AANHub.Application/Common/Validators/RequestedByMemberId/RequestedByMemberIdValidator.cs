e4345116c788f1ec0f3dc2a1870aca0e7cd09272
Saw Naw <saw.naw@digital.education.gov.uk>
2023-05-10 09:51:03 +0100

    CSP-764 Consistently use the header name X-RequestedByMemberId instead of similar variants

--- src/SFA.DAS.AANHub.Application/Common/Validators/RequestedByMemberId/RequestedByMemberIdValidator.cs
+++ src/SFA.DAS.AANHub.Application/Common/Validators/RequestedByMemberId/RequestedByMemberIdValidator.cs
@@ -6,17 +6,17 @@ namespace SFA.DAS.AANHub.Application.Common.Validators.RequestedByMemberId
 {
     public class RequestedByMemberIdValidator : AbstractValidator<IRequestedByMemberId>
     {
-        private const string RequestedByUserHeaderEmptyErrorMessage = "X-RequestedByUser header is empty";
-        private const string RequestedByUserIdNotFoundMessage = "Could not find a valid active user ID matching the X-RequestedByUser header";
+        private const string RequestedByMemberHeaderEmptyErrorMessage = "X-RequestedByMemberId header is empty";
+        private const string RequestedByMemberIdNotFoundMessage = "Could not find a valid active user ID matching the X-RequestedByMemberId header";
         public RequestedByMemberIdValidator(IMembersReadRepository membersReadRepository) => RuleFor(x => x.RequestedByMemberId)
                 .Cascade(CascadeMode.Stop)
                 .NotEmpty()
-                .WithMessage(RequestedByUserHeaderEmptyErrorMessage)
+                .WithMessage(RequestedByMemberHeaderEmptyErrorMessage)
                 .MustAsync(async (requestedByMemberId, cancellation) =>
                 {
                     var member = await membersReadRepository.GetMember(requestedByMemberId);
                     return member is { Status: MembershipStatus.Live };
                 })
-                .WithMessage(RequestedByUserIdNotFoundMessage);
+                .WithMessage(RequestedByMemberIdNotFoundMessage);
     }
 }
