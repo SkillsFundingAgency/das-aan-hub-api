3f170d92a61842ebd6ab5cae834847ce00ea0cbb
Deven Shah <devenshah1974@gmail.com>
2023-05-18 14:41:37 +0100

    Added ping endpoint which accepts a header

---
+++ src/SFA.DAS.AANHub.Api/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.AANHub.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index([FromHeader(Name = "X-RequestedByMemberId")] string requestedByMemberId)
+    {
+        return Ok(new { requestedByMemberId });
+    }
+}
