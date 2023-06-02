66e665a89b527b0fbb6ceb73190448c2173675a6
MarkFCain <mark.cain@digital.education.gov.uk>
2023-06-02 14:10:41 +0100

    CSP-808 fix to deal with nulls in CalendarEventSummary and sql mapping

--- src/SFA.DAS.AANHub.Domain/Models/CalendarEventSummary.cs
+++ src/SFA.DAS.AANHub.Domain/Models/CalendarEventSummary.cs
@@ -8,9 +8,9 @@ public class CalendarEventSummary
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
     public string Description { get; set; } = null!;
-    public string Summary { get; set; } = null!;
-    public string Location { get; set; } = null!;
-    public string Postcode { get; set; } = null!;
+    public string? Summary { get; set; }
+    public string? Location { get; set; }
+    public string? Postcode { get; set; }
     public double? Longitude { get; set; }
     public double? Latitude { get; set; }
     public double? Distance { get; set; }
