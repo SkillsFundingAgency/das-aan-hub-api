dfb19cdd6dd4a58361d816a905684943b94fcc30
Deven Shah <devenshah1974@gmail.com>
2023-06-06 19:36:14 +0100

    CSP-768 Implements Get Attendances endpoint

---
+++ src/SFA.DAS.AANHub.Application/Attendances/Queries/GetMemberAttendances/GetMemberAttendancesQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using SFA.DAS.AANHub.Application.Mediatr.Responses;
+using SFA.DAS.AANHub.Domain.Common;
+
+namespace SFA.DAS.AANHub.Application.Attendances.Queries.GetMemberAttendances;
+
+public record GetMemberAttendancesQuery(Guid MemberId, DateTime? FromDate, DateTime? ToDate)
+    : IRequest<ValidatedResponse<GetMemberAttendancesQueryResult>>;
+
+public record GetMemberAttendancesQueryResult(List<Attendance> Attendances);
+
+public record Attendance(Guid CalendarEventId, EventFormat EventFormat, DateTime CalendarEventStartDate, string EventDescription);
