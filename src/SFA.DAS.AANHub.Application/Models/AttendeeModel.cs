edbdd98f21618222172ab64836c65fa0b7d0bf31
SawNawDfE <119664159+SawNawDfE@users.noreply.github.com>
2023-06-19 17:24:52 +0100

    CSP-694 Include AddedDate in AttendeeModel (#96)
    Co-authored-by: Saw Naw <waterchan@gmail.com>

--- src/SFA.DAS.AANHub.Application/Models/AttendeeModel.cs
+++ src/SFA.DAS.AANHub.Application/Models/AttendeeModel.cs
@@ -2,8 +2,7 @@
 
 namespace SFA.DAS.AANHub.Application.Models;
 
-public record AttendeeModel(Guid MemberId, string UserType, string MemberName)
+public record AttendeeModel(Guid MemberId, string UserType, string MemberName, DateTime? AddedDate)
 {
-    public static implicit operator AttendeeModel(Attendance source) => new(source.MemberId, source.Member.UserType, source.Member.FullName);
+    public static implicit operator AttendeeModel(Attendance source) => new(source.MemberId, source.Member.UserType, source.Member.FullName, source.AddedDate);
 }
-
