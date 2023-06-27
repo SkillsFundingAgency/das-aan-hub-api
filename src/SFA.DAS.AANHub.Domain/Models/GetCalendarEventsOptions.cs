d881f3127a4bb2410812e8cb4656efda86a29f98
MarkFCain <mark.cain@digital.education.gov.uk>
2023-06-27 08:44:23 +0100

    CSP-804 removing code smell in GetCalendarEventsOptions around potential null values

--- src/SFA.DAS.AANHub.Domain/Models/GetCalendarEventsOptions.cs
+++ src/SFA.DAS.AANHub.Domain/Models/GetCalendarEventsOptions.cs
@@ -6,9 +6,9 @@ public class GetCalendarEventsOptions
     public Guid MemberId { get; set; }
     public DateTime? FromDate { get; set; }
     public DateTime? ToDate { get; set; }
-    public List<EventFormat> EventFormats { get; set; }
-    public List<int> CalendarIds { get; set; }
-    public List<int> RegionIds { get; set; }
+    public List<EventFormat> EventFormats { get; set; } = new List<EventFormat>();
+    public List<int> CalendarIds { get; set; } = new List<int>();
+    public List<int> RegionIds { get; set; } = new List<int>();
     public int Page { get; set; }
     public int PageSize { get; set; }
 }
