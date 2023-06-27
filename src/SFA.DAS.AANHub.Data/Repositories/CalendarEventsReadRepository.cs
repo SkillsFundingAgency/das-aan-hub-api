f3453ba6e812032949b67ad1f656b8d2f3a473c6
MarkFCain <mark.cain@digital.education.gov.uk>
2023-06-27 09:24:46 +0100

    CSP-804 indented SQL to make it more readable

--- src/SFA.DAS.AANHub.Data/Repositories/CalendarEventsReadRepository.cs
+++ src/SFA.DAS.AANHub.Data/Repositories/CalendarEventsReadRepository.cs
@@ -31,48 +31,48 @@ internal class CalendarEventsReadRepository : ICalendarEventsReadRepository
         var regions = GenerateRegionsSql(options.RegionIds);
 
         var sql = $@"select	               
-                             CE.Id as CalendarEventId, 
-                             COUNT(*) OVER () TotalCount,
-                             C.CalendarName,
-                             C.Id as CalendarId,
-                             CE.RegionId,
-                             CE.EventFormat, 
-                             CE.StartDate as [Start], 
-                             CE.EndDate as [End],
-                             CE.Title,
-                             CE.[Description], 
-                             CE.Summary,
-                             CE.[Location],   
-                             CE.Postcode,
-                             CE.Latitude,
-                             CE.Longitude,
-                             CASE  WHEN (EmployerDetails.Latitude is null) THEN null
-                           WHEN (EmployerDetails.Longitude is null) THEN null
-                           WHEN (CE.Latitude is null OR CE.Longitude is null) THEN null
-                          ELSE
-                           ROUND(geography::Point(CE.Latitude, CE.Longitude, 4326)
-                         .STDistance(geography::Point(convert(float,EmployerDetails.Latitude), convert(float,EmployerDetails.Longitude), 4326)) * 0.0006213712,1) END
-                 as Distance,
-                             ISNULL(A.IsAttending, 0) AS IsAttending
-                                from CalendarEvent CE inner join Calendar C on CE.CalendarId = C.Id
-                                LEFT OUTER JOIN (
-                                 SELECT MemberId
-                                          ,MAX(CASE WHEN ProfileId = {Constants.ProfileDataId.Latitude} THEN ProfileValue ELSE null END) Latitude
-                                          ,MAX(CASE WHEN ProfileId = {Constants.ProfileDataId.Longitude} THEN ProfileValue ELSE null END) Longitude
-                                    FROM MemberProfile mp1
-                                    WHERE MemberId = '{options.MemberId}'
-                                    GROUP BY MemberId
-                                 ) EmployerDetails on EmployerDetails.MemberId = '{options.MemberId}'
-                                LEFT outer join Attendance A on A.CalendarEventId = CE.Id and A.MemberId = '{options.MemberId}'
-                                WHERE CE.IsActive = 1
-                                AND CE.StartDate >= convert(date,'{options.FromDate?.ToString("yyyy-MM-dd")}') 
-                                AND CE.EndDate < convert(date,dateadd(day,1,'{options.ToDate?.ToString("yyyy-MM-dd")}'))
-                                {eventFormats}
-                                {eventTypes}
-                                {regions}
-                                Order by CE.StartDate
-                                OFFSET {(options.Page - 1) * options.PageSize} ROWS 
-                                FETCH NEXT {options.PageSize} ROWS ONLY";
+ CE.Id as CalendarEventId, 
+ COUNT(*) OVER () TotalCount,
+ C.CalendarName,
+ C.Id as CalendarId,
+ CE.RegionId,
+ CE.EventFormat, 
+ CE.StartDate as [Start], 
+ CE.EndDate as [End],
+ CE.Title,
+ CE.[Description], 
+ CE.Summary,
+ CE.[Location],   
+ CE.Postcode,
+ CE.Latitude,
+ CE.Longitude,
+ CASE   WHEN (EmployerDetails.Latitude is null) THEN null
+        WHEN (EmployerDetails.Longitude is null) THEN null
+        WHEN (CE.Latitude is null OR CE.Longitude is null) THEN null
+    ELSE
+    ROUND(geography::Point(CE.Latitude, CE.Longitude, 4326)
+    .STDistance(geography::Point(convert(float,EmployerDetails.Latitude), convert(float,EmployerDetails.Longitude), 4326)) * 0.0006213712,1) END
+    as Distance,
+ ISNULL(A.IsAttending, 0) AS IsAttending
+ FROM CalendarEvent CE INNER JOIN Calendar C ON CE.CalendarId = C.Id
+ LEFT OUTER JOIN (
+    SELECT MemberId,
+        MAX(CASE WHEN ProfileId = {Constants.ProfileDataId.Latitude} THEN ProfileValue ELSE null END) Latitude,
+        MAX(CASE WHEN ProfileId = {Constants.ProfileDataId.Longitude} THEN ProfileValue ELSE null END) Longitude
+    FROM MemberProfile mp1
+    WHERE MemberId = '{options.MemberId}'
+    GROUP BY MemberId
+    ) EmployerDetails on EmployerDetails.MemberId = '{options.MemberId}'
+ LEFT outer join Attendance A on A.CalendarEventId = CE.Id and A.MemberId = '{options.MemberId}'
+ WHERE CE.IsActive = 1
+ AND CE.StartDate >= convert(date,'{options.FromDate?.ToString("yyyy-MM-dd")}') 
+ AND CE.EndDate < convert(date,dateadd(day,1,'{options.ToDate?.ToString("yyyy-MM-dd")}'))
+ {eventFormats}
+ {eventTypes}
+ {regions}
+ Order by CE.StartDate
+ OFFSET {(options.Page - 1) * options.PageSize} ROWS 
+ FETCH NEXT {options.PageSize} ROWS ONLY";
 
 
         var calendarEvents = await _aanDataContext.CalendarEventSummaries!
