4f73b3874a956b83d143a7d74b313d7932677374
Alan Burns <adburns57@gmail.com>
2023-08-02 14:18:18 +0100

    CSP 870 AAN Member Profile Preferences (#99)
    * CSP-870 Database changes for Member Preferences
    
    * UNIQUE INDEX

---
+++ src/SFA.DAS.AANHub.Database/Tables/MemberPreference.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[MemberPreference]
+(
+    [Id] BIGINT IDENTITY(1,1) NOT NULL,
+    [MemberId] UNIQUEIDENTIFIER NOT NULL, 
+    [PreferenceId] INT NOT NULL, 
+    [AllowSharing] BIT DEFAULT 0
+    CONSTRAINT [PK_MemberPreference] PRIMARY KEY ([Id])
+    CONSTRAINT [FK_MemberPreference_Member] FOREIGN KEY ([MemberId]) REFERENCES [Member]([Id])
+    CONSTRAINT [FK_MemberPreference_Preference] FOREIGN KEY ([PreferenceId]) REFERENCES [Preference]([Id])
+);
+GO
+
+CREATE UNIQUE INDEX IXU_MemberPreference ON [MemberPreference] ([MemberId], [PreferenceId])
+INCLUDE ([AllowSharing]);
+GO
+
+
---
+++ src/SFA.DAS.AANHub.Database/Tables/Preference.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Preference]
+(
+    [Id] INT NOT NULL,
+    [Group] NVARCHAR(200) NOT NULL 
+    CONSTRAINT [PK_Preference] PRIMARY KEY ([Id])
+)
