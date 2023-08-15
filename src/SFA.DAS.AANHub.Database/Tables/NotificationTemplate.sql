372030f4e14b97757a1549a36eef3ae98a2e0d6b
Alan Burns <adburns57@gmail.com>
2023-08-15 15:55:00 +0100

    remove MergeFields from NotificationTemplate

--- src/SFA.DAS.AANHub.Database/Tables/NotificationTemplate.sql
+++ src/SFA.DAS.AANHub.Database/Tables/NotificationTemplate.sql
@@ -3,7 +3,6 @@
     [Id] BIGINT NOT NULL, 
     [Description] NVARCHAR(200) NOT NULL, 
     [TemplateName] NVARCHAR(200) NOT NULL, 
-    [MergeFields] NVARCHAR(MAX) NOT NULL, 
     [IsActive] BIT NOT NULL DEFAULT 1, 
     CONSTRAINT [PK_NotificationTemplate] PRIMARY KEY ([Id])        
 )
