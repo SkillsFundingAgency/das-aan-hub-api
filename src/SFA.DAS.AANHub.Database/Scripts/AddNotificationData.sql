95e918f549e75426226be380c7d2ed43030265c8
Alan Burns <adburns57@gmail.com>
2023-08-15 16:03:14 +0100

    ... and from the post-deployment

--- src/SFA.DAS.AANHub.Database/Scripts/AddNotificationData.sql
+++ src/SFA.DAS.AANHub.Database/Scripts/AddNotificationData.sql
@@ -3,17 +3,16 @@
     [Id] BIGINT NOT NULL, 
     [Description] NVARCHAR(200) NOT NULL, 
     [TemplateName] NVARCHAR(200) NOT NULL, 
-    [MergeFields] NVARCHAR(MAX) NOT NULL, 
     [IsActive] BIT NOT NULL,   
 );
 
 
 INSERT INTO #NotificationTemplate
 VALUES 
-(1,'ask for industry advice','AANIndustryAdvice','"contact","link"',1),
-(2,'ask for help with a network activity','AANAskForHelp','"contact","link"',1),
-(3,'request a case study','AANRequestCaseStudy','"contact","link"',1),
-(4,'get in touch after meeting at a network event','AANGetInTouch','"contact","link"',1);
+(1,'ask for industry advice','AANIndustryAdvice',1),
+(2,'ask for help with a network activity','AANAskForHelp',1),
+(3,'request a case study','AANRequestCaseStudy',1),
+(4,'get in touch after meeting at a network event','AANGetInTouch',1);
 
 
 MERGE INTO [dbo].[NotificationTemplate] src
@@ -22,11 +21,10 @@ ON (src.[id] = upd.[Id])
 WHEN MATCHED THEN 
 UPDATE SET src.[Description] = upd.[Description],
            src.[TemplateName] = upd.[TemplateName],
-           src.[MergeFields] = upd.[MergeFields],
            src.[IsActive] = upd.[IsActive]
 WHEN NOT MATCHED THEN 
-INSERT ([Id],[Description],[TemplateName],[MergeFields],[IsActive])
-VALUES (upd.[Id],upd.[Description],upd.[TemplateName],upd.[MergeFields],upd.[IsActive])
+INSERT ([Id],[Description],[TemplateName],[IsActive])
+VALUES (upd.[Id],upd.[Description],upd.[TemplateName],upd.[IsActive])
 WHEN NOT MATCHED BY SOURCE THEN 
 UPDATE SET src.[IsActive] = 0
 ;
