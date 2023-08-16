50692ea15daa54de01f273cd76dd5f47618abe10
Deven Shah <devenshah1974@gmail.com>
2023-08-16 10:48:31 +0100

    CSP-939 Added missing column

--- src/SFA.DAS.AANHub.Database/Tables/Notification.sql
+++ src/SFA.DAS.AANHub.Database/Tables/Notification.sql
@@ -1,13 +1,14 @@
 ﻿CREATE TABLE [dbo].[Notification]
 (
     [Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
-    [MemberId] UNIQUEIDENTIFIER NOT NULL, 
-    [TemplateName] NVARCHAR(200) NOT NULL, 
-    [Tokens] NVARCHAR(MAX) NOT NULL, 
+    [MemberId] UNIQUEIDENTIFIER NOT NULL,
+    [TemplateName] NVARCHAR(200) NOT NULL,
+    [Tokens] NVARCHAR(MAX) NOT NULL,
     [CreatedBy] UNIQUEIDENTIFIER NULL,
     [CreatedDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
-    [SendAfterTime] DATETIME NULL, 
-    [SentTime] DATETIME NULL, 
-    CONSTRAINT [PK_Notification] PRIMARY KEY ([Id]),    
+    [SendAfterTime] DATETIME NULL,
+    [SentTime] DATETIME NULL,
+    [IsSystem] BIT NOT NULL,
+    CONSTRAINT [PK_Notification] PRIMARY KEY ([Id]),
     CONSTRAINT [FK_Notifications_Member] FOREIGN KEY ([MemberId]) REFERENCES [Member]([Id])
 )
