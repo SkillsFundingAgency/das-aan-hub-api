b67697795e5f826ce03d5738ee74e6279a62005d
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-08-22 13:49:07 +0100

    CSO-950 Add notification record when event attendance status changes
    CSP-950 Make two models one
    
    CSP-950/CSP-951 Add notification record when user registers/cancels attendance at an event
    
    CSP-950 remove unused parameter
    
    CSP-950 Add unit test coverage for templateName logic
    
    CSP-950 remove unused parameter

---
+++ src/SFA.DAS.AANHub.Domain/Models/EventAttendanceEmailTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SFA.DAS.AANHub.Domain.Models;
+public class EventAttendanceEmailTemplate
+{
+    public EventAttendanceEmailTemplate(string firstName, string lastName, string eventName, string date, string time)
+    {
+        Contact = $"{firstName} {lastName}";
+        EventName = eventName;
+        Date = date;
+        Time = time;
+    }
+
+    [JsonPropertyName("contact")]
+    public string Contact { get; set; }
+
+    [JsonPropertyName("eventname")]
+    public string EventName { get; set; }
+
+    [JsonPropertyName("date")]
+    public string Date { get; set; }
+
+    [JsonPropertyName("time")]
+    public string Time { get; set; }
+}
