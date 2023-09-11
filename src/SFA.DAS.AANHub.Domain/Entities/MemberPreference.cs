e05a1c72bb171a1dc5304cbb0dfa7a88e1feb46e
Sarah Duncan <sarah.duncan@education.gov.uk>
2023-09-11 14:26:18 +0100

    CSP-1024 change int to long for Id

--- src/SFA.DAS.AANHub.Domain/Entities/MemberPreference.cs
+++ src/SFA.DAS.AANHub.Domain/Entities/MemberPreference.cs
@@ -1,7 +1,7 @@
 ﻿namespace SFA.DAS.AANHub.Domain.Entities;
 public class MemberPreference
 {
-    public int Id { get; set; }
+    public long Id { get; set; }
     public Guid MemberId { get; set; }
     public int PreferenceId { get; set; }
     public bool AllowSharing { get; set; }
