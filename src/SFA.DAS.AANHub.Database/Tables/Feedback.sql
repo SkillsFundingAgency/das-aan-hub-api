423916428ec535a8505a2a62f04a94c25f7eece5
Alan Burns <adburns57@gmail.com>
2023-09-11 09:54:32 +0100

    CSP-1026 Database changes for leaving members (#126)
    * CSP-1026_database_changes_for_leaving
    
    * Keep feedback tables as these are removed by CSP-1025-RemoveFeedbackAndPartnerComponents
    
    * finish the revert

---
+++ src/SFA.DAS.AANHub.Database/Tables/Feedback.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Feedback]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    [AttendanceId] UNIQUEIDENTIFIER NOT NULL,
+    [AttributeId] INT NOT NULL, 
+    [AttributeValue] NVARCHAR(max) NOT NULL,
+    CONSTRAINT [FK_Feedback_AttendanceId] FOREIGN KEY ([AttendanceId]) REFERENCES [Attendance]([Id]),     
+    CONSTRAINT [FK_Feedback_AttributeId] FOREIGN KEY ([AttributeId]) REFERENCES [FeedbackAttribute]([Id])    
+)
+GO
+
+CREATE UNIQUE INDEX IXU_Feedback_AttendanceId_AttributeId ON [Feedback]([AttendanceId], [AttributeId])
+INCLUDE ([AttributeValue])
+GO
