05c37342e697ec4e113b764c333b9dab1fd679cb
Deven Shah <devenshah1974@gmail.com>
2023-10-04 23:36:25 +0100

    CSP-1077 Implemented create calendar event command handler

---
+++ src/SFA.DAS.AANHub.Application/CalendarEvents/Commands/CreateCalendarEvent/CreateCalendarEventCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using MediatR;
+using SFA.DAS.AANHub.Domain.Entities;
+using SFA.DAS.AANHub.Domain.Interfaces;
+using SFA.DAS.AANHub.Domain.Interfaces.Repositories;
+
+namespace SFA.DAS.AANHub.Application.CalendarEvents.Commands.CreateCalendarEvent;
+
+public class CreateCalendarEventCommandHandler : IRequestHandler<CreateCalendarEventCommand, CreateCalendarEventCommandResult>
+{
+    private readonly ICalendarEventsWriteRepository _calendarEventWriteRepository;
+    private readonly IAuditWriteRepository _auditWriteRepository;
+    private readonly IAanDataContext _aanDataContext;
+
+    public CreateCalendarEventCommandHandler(ICalendarEventsWriteRepository calendarEventWriteRepository, IAuditWriteRepository auditWriteRepository, IAanDataContext aanDataContext)
+    {
+        _calendarEventWriteRepository = calendarEventWriteRepository;
+        _auditWriteRepository = auditWriteRepository;
+        _aanDataContext = aanDataContext;
+    }
+
+    public async Task<CreateCalendarEventCommandResult> Handle(CreateCalendarEventCommand request, CancellationToken cancellationToken)
+    {
+        CalendarEvent calendarEvent = request;
+        _calendarEventWriteRepository.Create(calendarEvent);
+
+        _auditWriteRepository.Create(new Audit
+        {
+            Action = "Create",
+            ActionedBy = request.AdminMemberId,
+            AuditTime = DateTime.UtcNow,
+            After = JsonSerializer.Serialize(request),
+            Resource = nameof(CalendarEvent)
+        });
+
+        await _aanDataContext.SaveChangesAsync(cancellationToken);
+
+        return new(calendarEvent.Id);
+    }
+}
---
+++ src/SFA.DAS.AANHub.Data/Repositories/CalendarEventsWriteRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using SFA.DAS.AANHub.Domain.Entities;
+using SFA.DAS.AANHub.Domain.Interfaces.Repositories;
+
+namespace SFA.DAS.AANHub.Data.Repositories;
+
+[ExcludeFromCodeCoverage]
+internal class CalendarEventsWriteRepository : ICalendarEventsWriteRepository
+{
+    private readonly AanDataContext _aanDataContext;
+
+    public CalendarEventsWriteRepository(AanDataContext aanDataContext) => _aanDataContext = aanDataContext;
+
+    public void Create(CalendarEvent calendarEvent) => _aanDataContext.CalendarEvents.Add(calendarEvent);
+}
