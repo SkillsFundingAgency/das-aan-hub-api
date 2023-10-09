d370cd1a3e94436f26990da3e9540580c5d6cac8
MarkFCain <mark.cain@digital.education.gov.uk>
2023-10-09 09:27:47 +0100

    CSP-1078 conforming urlregex to match web urlregex

--- src/SFA.DAS.AANHub.Application/Constants.cs
+++ src/SFA.DAS.AANHub.Application/Constants.cs
@@ -1,19 +1,18 @@
-﻿namespace SFA.DAS.AANHub.Application
-{
-    public static class Constants
-    {
-        public static class RegularExpressions
-        {
-            public const string UrlRegex = @"^(?:[Hh][Tt][Tt][Pp]([Ss])?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
-            public const string EmailRegex = @"^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z0-9_](-?[a-zA-Z0-9_])*(\.[a-zA-Z0-9](-?[a-zA-Z0-9])*)+$";
-            public const string PostcodeRegex = @"^[a-zA-z]{1,2}\d[a-zA-z\d]?\s*\d[a-zA-Z]{2}$";
-            public const string ExcludedCharactersRegex = @"^[^@#$^=+\\\/<>%]*$";
-        }
+﻿namespace SFA.DAS.AANHub.Application;
 
-        public static class MembershipStatus
-        {
-            public const string Withdrawn = "withdrawn";
-            public const string Deleted = "deleted";
-        }
+public static class Constants
+{
+    public static class RegularExpressions
+    {
+        public const string UrlRegex = @"^[Hh][Tt][Tt][Pp][Ss]://(?:[Ww][Ww][Ww]\\.)?[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}(/\\S*)?$";
+        public const string EmailRegex = @"^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z0-9_](-?[a-zA-Z0-9_])*(\.[a-zA-Z0-9](-?[a-zA-Z0-9])*)+$";
+        public const string PostcodeRegex = @"^[a-zA-z]{1,2}\d[a-zA-z\d]?\s*\d[a-zA-Z]{2}$";
+        public const string ExcludedCharactersRegex = @"^[^@#$^=+\\\/<>%]*$";
+    }
+
+    public static class MembershipStatus
+    {
+        public const string Withdrawn = "withdrawn";
+        public const string Deleted = "deleted";
     }
 }
