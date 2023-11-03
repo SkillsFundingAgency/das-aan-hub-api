dff28dc6f34296e121068ddde0a345759a0b481f
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-11-03 13:07:00 +0000

    PR comments resolved

--- src/SFA.DAS.AANHub.Application.UnitTests/Admins/Commands/CreateAdminMember/CreateAdminMemberCommandHandlerTests.cs
+++ src/SFA.DAS.AANHub.Application.UnitTests/Admins/Commands/CreateAdminMember/CreateAdminMemberCommandHandlerTests.cs
@@ -4,6 +4,7 @@ using Moq;
 using NUnit.Framework;
 using SFA.DAS.AANHub.Application.Admins.Commands.CreateAdminMember;
 using SFA.DAS.AANHub.Domain.Entities;
+using SFA.DAS.AANHub.Domain.Interfaces;
 using SFA.DAS.AANHub.Domain.Interfaces.Repositories;
 using SFA.DAS.Testing.AutoFixture;
 using static SFA.DAS.AANHub.Domain.Common.Constants;
@@ -21,7 +22,7 @@ public class CreateAdminMemberCommandHandlerTests
         CreateAdminMemberCommandHandler sut,
         CreateAdminMemberCommand command)
     {
-        membersReadRepository.Setup(p => p.GetMemberByEmail(command.Email)).ReturnsAsync((Member)null);
+        membersReadRepository.Setup(p => p.GetMemberByEmail(command.Email!)).ReturnsAsync((Member)null!);
 
         var response = await sut.Handle(command, new CancellationToken());
         response.Result.MemberId.Should().Be(command.MemberId);
@@ -34,13 +35,22 @@ public class CreateAdminMemberCommandHandlerTests
     [Test]
     [MoqAutoData]
     public async Task Handle_ExistingAdmin(
+        [Frozen] Mock<IMembersWriteRepository> membersWriteRepository,
         [Frozen] Mock<IMembersReadRepository> membersReadRepository,
+        [Frozen] Mock<IAuditWriteRepository> auditWriteRepository,
+        [Frozen] Mock<IAanDataContext> anDataContext,
         CreateAdminMemberCommandHandler sut,
         CreateAdminMemberCommand command)
     {
-        membersReadRepository.Setup(p => p.GetMemberByEmail(command.Email)).ReturnsAsync(command);
+        membersReadRepository.Setup(p => p.GetMemberByEmail(command.Email!)).ReturnsAsync(command);
 
         var response = await sut.Handle(command, new CancellationToken());
         response.Result.MemberId.Should().Be(command.MemberId);
+
+        membersWriteRepository.Verify(p => p.Create(It.Is<Member>(x => x.Id == command.MemberId)), Times.Never);
+        membersWriteRepository.Verify(p => p.Create(It.Is<Member>(x => x.UserType == MembershipUserType.Admin)), Times.Never);
+        auditWriteRepository.Verify(p => p.Create(It.Is<Audit>(x => x.ActionedBy == command.MemberId)), Times.Never);
+
+        anDataContext.Verify(p => p.SaveChangesAsync(new CancellationToken()), Times.Never);
     }
 }
