8ef4bf1b3cb2d84b58ec58b1bcd5cab32e53f8a5
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-11-28 08:39:31 +0000

    CSP-1123 Fixed PR comments

--- src/SFA.DAS.AANHub.Application.UnitTests/MemberProfiles/Commands/PutMemberProfile/UpdateMemberProfilesCommandHandlerTests.cs
+++ src/SFA.DAS.AANHub.Application.UnitTests/MemberProfiles/Commands/PutMemberProfile/UpdateMemberProfilesCommandHandlerTests.cs
@@ -1,7 +1,7 @@
-﻿using System.Text.Json;
-using AutoFixture.NUnit3;
+﻿using AutoFixture.NUnit3;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.AANHub.Application.Common;
 using SFA.DAS.AANHub.Application.MemberProfiles.Commands.PutMemberProfile;
 using SFA.DAS.AANHub.Domain.Entities;
 using SFA.DAS.AANHub.Domain.Interfaces;
@@ -11,23 +11,13 @@ using SFA.DAS.Testing.AutoFixture;
 namespace SFA.DAS.AANHub.Application.UnitTests.MemberProfiles.Commands.PutMemberProfile;
 public class UpdateMemberProfilesCommandHandlerTests
 {
-    [Test, MoqAutoData]
-    public async Task Handle_MatchingMemberUpdatesProfilesAndPreferences(
-        [Frozen] Mock<IAanDataContext> aanDataContext,
-        [Frozen] Mock<IMembersWriteRepository> membersWriteRepository,
-        [Frozen] Mock<IAuditWriteRepository> auditWriteRepository,
-        UpdateMemberProfilesCommandHandler sut)
-    {
-        Member existingMember = new();
-        existingMember.Id = Guid.NewGuid();
-
-        existingMember.MemberProfiles = new List<MemberProfile>()
+    private static List<MemberProfile> memberProfiles = new List<MemberProfile>()
         {   new MemberProfile { ProfileId = 41, ProfileValue = "ToBeUpdated" },
             new MemberProfile { ProfileId = 42, ProfileValue = "NotToBeUpdated" },
             new MemberProfile { ProfileId = 44, ProfileValue = "ToBeSetToNull" }
         };
 
-        var updateProfileModel = new List<UpdateProfileModel>()
+    private static List<UpdateProfileModel> updateProfileModels = new List<UpdateProfileModel>()
         {
             new UpdateProfileModel { MemberProfileId = 41, Value = "UpdatedValue" },
             new UpdateProfileModel { MemberProfileId = 42, Value = "ToBeInsertedValue" },
@@ -35,37 +25,172 @@ public class UpdateMemberProfilesCommandHandlerTests
             new UpdateProfileModel { MemberProfileId = 45, Value = "ToBeNotNull" }
         };
 
-        existingMember.MemberPreferences = new List<MemberPreference>()
+    private static List<MemberPreference> memberPreferences = new List<MemberPreference>()
         {
             new MemberPreference{ PreferenceId =1, AllowSharing =true },
             new MemberPreference{ PreferenceId =2, AllowSharing =false }
         };
 
-        var updatePreferenceModel = new List<UpdatePreferenceModel>()
+    private static List<UpdatePreferenceModel> updatePreferenceModels = new List<UpdatePreferenceModel>()
         {
             new UpdatePreferenceModel { PreferenceId = 1, Value = false },
             new UpdatePreferenceModel { PreferenceId = 2, Value = true }
         };
+    private static UpdateMemberProfilesCommand command = null!;
+    private static Member existingMember = new();
 
-        UpdateMemberProfilesCommand command = new(existingMember.Id, updateProfileModel, updatePreferenceModel);
+    [Test, MoqAutoData]
+    public async Task Handle_ShouldInvokeSaveChangesAsync(
+      [Frozen] Mock<IAanDataContext> aanDataContext,
+      [Frozen] Mock<IMembersWriteRepository> membersWriteRepository,
+      UpdateMemberProfilesCommandHandler sut)
+    {
+        // Arrange
+        existingMember.Id = Guid.NewGuid();
+        existingMember.MemberProfiles = memberProfiles;
+        existingMember.MemberPreferences = memberPreferences;
+        membersWriteRepository.Setup(x => x.Get(It.IsAny<Guid>())).ReturnsAsync(() => existingMember);
+        command = new(existingMember.Id, updateProfileModels, updatePreferenceModels);
 
-        membersWriteRepository.Setup(x => x.Get(existingMember.Id)).ReturnsAsync(() => existingMember);
-
+        // Act
         await sut.Handle(command, new CancellationToken());
 
-        membersWriteRepository.Verify(p => p.Create(It.IsAny<Member>()), Times.Never);
+        // Assert
         aanDataContext.Verify(a => a.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
-        var serializedExistingMemberProfile = JsonSerializer.Serialize(new List<MemberProfile>()
-        {   new MemberProfile { ProfileId = 41, ProfileValue = "ToBeUpdated" },
-            new MemberProfile { ProfileId = 42, ProfileValue = "NotToBeUpdated" },
-            new MemberProfile { ProfileId = 44, ProfileValue = "ToBeSetToNull" }
-        });
+    }
+
+    public async Task Handle_ShouldInvokeGet(
+      [Frozen] Mock<IMembersWriteRepository> membersWriteRepository,
+      UpdateMemberProfilesCommandHandler sut)
+    {
+        // Arrange
+        existingMember.Id = Guid.NewGuid();
+        existingMember.MemberProfiles = memberProfiles;
+        existingMember.MemberPreferences = memberPreferences;
+        membersWriteRepository.Setup(x => x.Get(It.IsAny<Guid>())).ReturnsAsync(() => existingMember);
+        command = new(existingMember.Id, updateProfileModels, updatePreferenceModels);
+
+        // Act
+        await sut.Handle(command, new CancellationToken());
+
+        // Assert
+        membersWriteRepository.Verify(a => a.Get(It.IsAny<Guid>()), Times.Once);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_MemberPreferencesIsEmpty_CreateShouldNotBeInvokedForMemberPreferences(
+          [Frozen] Mock<IAanDataContext> aanDataContext,
+          [Frozen] Mock<IMembersWriteRepository> membersWriteRepository,
+          [Frozen] Mock<IAuditWriteRepository> auditWriteRepository,
+          UpdateMemberProfilesCommandHandler sut)
+    {
+        // Arrange
+        existingMember.Id = Guid.NewGuid();
+        existingMember.MemberProfiles = memberProfiles;
+        existingMember.MemberPreferences = memberPreferences;
+        membersWriteRepository.Setup(x => x.Get(It.IsAny<Guid>())).ReturnsAsync(() => existingMember);
+        command = new(existingMember.Id, updateProfileModels, updatePreferenceModels);
+        command.MemberPreferences = new List<UpdatePreferenceModel>();
+
+        // Act
+        await sut.Handle(command, new CancellationToken());
+
+        // Assert
         auditWriteRepository.Verify(a => a.Create(
-                It.Is<Audit>(
-                    a => a.Action == "Put"
-                    && a.Before == serializedExistingMemberProfile
-                    && a.ActionedBy == existingMember.Id
-                    && a.Resource == nameof(MemberProfile))),
-                Times.Once);
+        It.Is<Audit>(
+            a => a.Action == "Put"
+            && a.Resource == nameof(MemberPreference))),
+        Times.Never);
+        auditWriteRepository.Verify(a => a.Create(
+        It.Is<Audit>(
+            a => a.Action == "Put"
+            && a.Resource == nameof(MemberProfile))),
+        Times.Once);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_MemberProfilesIsEmpty_CreateShouldNotBeInvokedForMemberProfile(
+      [Frozen] Mock<IAanDataContext> aanDataContext,
+      [Frozen] Mock<IMembersWriteRepository> membersWriteRepository,
+      [Frozen] Mock<IAuditWriteRepository> auditWriteRepository,
+      UpdateMemberProfilesCommandHandler sut)
+    {
+        // Arrange
+        existingMember.Id = Guid.NewGuid();
+        existingMember.MemberProfiles = memberProfiles;
+        existingMember.MemberPreferences = memberPreferences;
+        membersWriteRepository.Setup(x => x.Get(It.IsAny<Guid>())).ReturnsAsync(() => existingMember);
+        command = new(existingMember.Id, updateProfileModels, updatePreferenceModels);
+        command.MemberProfiles = new List<UpdateProfileModel>();
+
+        // Act
+        await sut.Handle(command, new CancellationToken());
+
+        // Assert
+        auditWriteRepository.Verify(a => a.Create(
+        It.Is<Audit>(
+            a => a.Action == "Put"
+            && a.Resource == nameof(MemberProfile))),
+        Times.Never);
+        auditWriteRepository.Verify(a => a.Create(
+        It.Is<Audit>(
+            a => a.Action == "Put"
+            && a.Resource == nameof(MemberPreference))),
+        Times.Once);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_MemberProfilesAndMemberPreferencesAreEmpty_CreateShouldNotBeInvoked([Frozen] Mock<IAanDataContext> aanDataContext,
+      [Frozen] Mock<IMembersWriteRepository> membersWriteRepository,
+      [Frozen] Mock<IAuditWriteRepository> auditWriteRepository,
+      UpdateMemberProfilesCommandHandler sut)
+    {
+        // Arrange
+        existingMember.Id = Guid.NewGuid();
+        existingMember.MemberProfiles = memberProfiles;
+        existingMember.MemberPreferences = memberPreferences;
+        membersWriteRepository.Setup(x => x.Get(It.IsAny<Guid>())).ReturnsAsync(() => existingMember);
+        command = new(existingMember.Id, updateProfileModels, updatePreferenceModels);
+        command.MemberProfiles = new List<UpdateProfileModel>();
+        command.MemberPreferences = new List<UpdatePreferenceModel>();
+
+        // Act
+        await sut.Handle(command, new CancellationToken());
+
+        // Assert
+        auditWriteRepository.Verify(a => a.Create(
+        It.Is<Audit>(
+            a => a.Action == "Put"
+            && a.Resource == nameof(MemberProfile))),
+        Times.Never);
+        auditWriteRepository.Verify(a => a.Create(
+        It.Is<Audit>(
+            a => a.Action == "Put"
+            && a.Resource == nameof(MemberPreference))),
+        Times.Never);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_OnHandlerSuccess_ReturnsSuccessCommandResult(
+        [Frozen] Mock<IMembersWriteRepository> membersWriteRepository,
+        UpdateMemberProfilesCommandHandler sut)
+    {
+        // Arrange
+        existingMember.Id = Guid.NewGuid();
+        existingMember.MemberProfiles = memberProfiles;
+        existingMember.MemberPreferences = memberPreferences;
+        membersWriteRepository.Setup(x => x.Get(It.IsAny<Guid>())).ReturnsAsync(() => existingMember);
+        command = new(existingMember.Id, updateProfileModels, updatePreferenceModels);
+
+        // Act
+        var result = await sut.Handle(command, new CancellationToken());
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.IsValidResponse, Is.EqualTo(true));
+            Assert.That(result.Result, Is.InstanceOf<SuccessCommandResult>());
+        });
     }
 }
