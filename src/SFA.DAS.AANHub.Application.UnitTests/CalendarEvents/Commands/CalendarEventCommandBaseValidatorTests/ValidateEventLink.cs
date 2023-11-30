cd9d4782ec74cc2b64fc553cc8cd1732c47f356e
Deven Shah <devenshah1974@gmail.com>
2023-11-30 17:46:25 +0000

    CSP-1087 Fix URL regex to match admin web

--- src/SFA.DAS.AANHub.Application.UnitTests/CalendarEvents/Commands/CalendarEventCommandBaseValidatorTests/ValidateEventLink.cs
+++ src/SFA.DAS.AANHub.Application.UnitTests/CalendarEvents/Commands/CalendarEventCommandBaseValidatorTests/ValidateEventLink.cs
@@ -19,7 +19,8 @@ public class ValidateEventLink
     [TestCase(EventFormat.Hybrid, 0, "https://www.google.com", true, null)]
     [TestCase(EventFormat.Hybrid, 0, "HTTPS://www.google.com", true, null)]
     [TestCase(EventFormat.Hybrid, 0, "HTTPS://WWW.google.com", true, null)]
-
+    [TestCase(EventFormat.Online, 0, "https://test-meet.google.com", true, null)]
+    [TestCase(EventFormat.Hybrid, 0, "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZGRmZC00NzlkLWFmZTktOTU4ZmFkMjA2ZDE1%thread.v2/0?context=%7b%22ad277c9-c60a-4da1-b5f3-b3b8Oid%22%3a%2209a4-c6-48-bc-ad60%22%7d", true, null)]
     public async Task Validate_EventLink_MustBeValidValue(EventFormat eventFormat, int length, string? eventLink, bool isValid, string? errorMessage)
     {
         var sut = CalendarEventCommandBaseValidatorBuilder.Create();
