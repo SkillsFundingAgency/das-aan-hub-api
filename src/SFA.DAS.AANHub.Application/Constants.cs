c138f0e560dd08de266e39fabdd9d071e2ea3e59
Deven Shah <devenshah1974@gmail.com>
2023-12-01 12:07:12 +0000

    CSP-1087 Fix URL regex to match admin web (#159)

--- src/SFA.DAS.AANHub.Application/Constants.cs
+++ src/SFA.DAS.AANHub.Application/Constants.cs
@@ -8,7 +8,7 @@ public static class Constants
     [ExcludeFromCodeCoverage]
     public static class RegularExpressions
     {
-        public const string UrlRegex = @"^(?:[Hh][Tt][Tt][Pp]([Ss])?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+        public const string UrlRegex = @"(?i)^https:\/\/[(www\.)?a-zA-Z0-9@:%._\+~#=-]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
         public const string EmailRegex = @"^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z0-9_](-?[a-zA-Z0-9_])*(\.[a-zA-Z0-9](-?[a-zA-Z0-9])*)+$";
         public const string PostcodeRegex = @"^[a-zA-z]{1,2}\d[a-zA-z\d]?\s*\d[a-zA-Z]{2}$";
         public const string ExcludedCharactersRegex = @"^[^@#$^=+\\\/<>%]*$";
