3ea3ebebe28c40a38e2bf8b0eddef0cebf1cd6e1
Alan Burns <adburns57@gmail.com>
2023-12-15 09:11:28 +0000

    Add the other Views on Audit

---
+++ src/SFA.DAS.AANHub.Database/Views/vAuditAttendance.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[vAuditAttendance] 
+AS
+SELECT [EntityId] [AttendanceId], [Id] [AuditId] , [AuditTime] , [Action], [ActionedBy]
+FROM [dbo].[Audit]
+WHERE [Resource] = 'Attendance';
+GO
---
+++ src/SFA.DAS.AANHub.Database/Views/vAuditMemberProfile.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[vAuditMemberProfile] 
+AS
+SELECT [EntityId] [MemberId], [Id] [AuditId] , [AuditTime] , [Action], [ActionedBy]
+FROM [dbo].[Audit]
+WHERE [Resource] = 'MemberProfile';
+GO
