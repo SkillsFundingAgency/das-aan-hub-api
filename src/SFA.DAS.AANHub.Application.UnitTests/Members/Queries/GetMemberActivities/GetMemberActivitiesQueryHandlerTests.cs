dbbf4f845411b72ead1494f6468eaba0fa17b11a
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2024-01-24 14:58:18 +0000

    CSP-1159 Fixed sonarcube coverage issues

--- src/SFA.DAS.AANHub.Application.UnitTests/Members/Queries/GetMemberActivities/GetMemberActivitiesQueryHandlerTests.cs
+++ src/SFA.DAS.AANHub.Application.UnitTests/Members/Queries/GetMemberActivities/GetMemberActivitiesQueryHandlerTests.cs
@@ -14,11 +14,19 @@ public class GetMemberActivitiesQueryHandlerTests
         [Frozen] Mock<IAttendancesReadRepository> attendancesReadRepository,
         [Frozen] Mock<IAuditReadRepository> auditReadRepository,
         GetMemberActivitiesQueryHandler sut,
-        List<Attendance> attendances,
         Audit audit,
         Guid memberId)
     {
         // Arrange
+        List<Attendance> attendances = new List<Attendance>()
+        {
+            new Attendance(){Id=Guid.NewGuid(),CalendarEventId=Guid.NewGuid(),AddedDate = DateTime.UtcNow.AddDays(-5),MemberId=memberId,IsAttending=true,CalendarEvent = new CalendarEvent(){ Title= "Event Title 1",Urn=null} },
+            new Attendance(){Id=Guid.NewGuid(),CalendarEventId=Guid.NewGuid(),AddedDate = DateTime.UtcNow.AddDays(-2),MemberId=memberId,IsAttending=true,CalendarEvent = new CalendarEvent(){ Title= "Event Title 2",Urn=null}},
+            new Attendance(){Id=Guid.NewGuid(),CalendarEventId=Guid.NewGuid(),AddedDate = DateTime.UtcNow,MemberId=memberId,IsAttending=true,CalendarEvent = new CalendarEvent(){ Title= "Event Title 3",Urn=null}},
+            new Attendance(){Id=Guid.NewGuid(),CalendarEventId=Guid.NewGuid(),AddedDate = DateTime.UtcNow.AddDays(2),MemberId=memberId,IsAttending=true,CalendarEvent = new CalendarEvent(){ Title= "Event Title 4",Urn=null}},
+            new Attendance(){Id=Guid.NewGuid(),CalendarEventId=Guid.NewGuid(),AddedDate = DateTime.UtcNow.AddDays(5),MemberId=memberId,IsAttending=true,CalendarEvent = new CalendarEvent(){ Title= "Event Title 5",Urn=null}},
+        };
+
         attendancesReadRepository.Setup(a => a.GetAttendances(It.IsAny<Guid>(), It.IsAny<DateTime>(), It.IsAny<DateTime>(), It.IsAny<CancellationToken>())).ReturnsAsync(attendances);
         auditReadRepository.Setup(a => a.GetLastAttendanceAuditByMemberId(It.IsAny<Guid>(), It.IsAny<CancellationToken>())).ReturnsAsync(audit);
 
@@ -27,17 +35,19 @@ public class GetMemberActivitiesQueryHandlerTests
 
         // Assert
         Assert.Multiple(() =>
-        {
-            Assert.That(result, Is.Not.Null);
-            Assert.That(result.Result, Is.InstanceOf<GetMemberActivitiesResult>());
-            Assert.That(result.Result.LastSignedUpDate, Is.EqualTo(audit.AuditTime));
-            Assert.That(result.Result.EventsPlanned, Is.Not.Null);
-            Assert.That(result.Result.EventsPlanned.Events, Is.Not.Null);
-            Assert.That(result.Result.EventsPlanned.EventsDateRange, Is.Not.Null);
-            Assert.That(result.Result.EventsAttended, Is.Not.Null);
-            Assert.That(result.Result.EventsAttended.Events, Is.Not.Null);
-            Assert.That(result.Result.EventsAttended.EventsDateRange, Is.Not.Null);
-        });
+            {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result.Result, Is.InstanceOf<GetMemberActivitiesResult>());
+                Assert.That(result.Result.LastSignedUpDate, Is.EqualTo(audit.AuditTime));
+                Assert.That(result.Result.EventsPlanned, Is.Not.Null);
+                Assert.That(result.Result.EventsPlanned.Events, Is.Not.Null);
+                Assert.That(result.Result.EventsPlanned.Events.Count, Is.EqualTo(2));
+                Assert.That(result.Result.EventsPlanned.EventsDateRange, Is.Not.Null);
+                Assert.That(result.Result.EventsAttended, Is.Not.Null);
+                Assert.That(result.Result.EventsAttended.Events, Is.Not.Null);
+                Assert.That(result.Result.EventsAttended.Events.Count, Is.EqualTo(3));
+                Assert.That(result.Result.EventsAttended.EventsDateRange, Is.Not.Null);
+            });
     }
 
     [Test, RecursiveMoqAutoData]
