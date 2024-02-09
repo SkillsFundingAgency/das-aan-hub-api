dd8063f4962d6166e99c3a02ea7573e9ff301650
MarkFCain <mark.cain@digital.education.gov.uk>
2024-02-09 14:15:42 +0000

    CSP-1266 removed post deploy script

--- src/SFA.DAS.AANHub.Database/Scripts/UpdateAttendanceAudit.sql
+++
@@ -1,6 +0,0 @@
-﻿
-  update A Set A.EntityId = CE.Id
-	from Attendance Att 
-	inner join CalendarEvent CE on Att.CalendarEventId = CE.Id
-	inner join Audit A on A.EntityId = Att.Id 
-	where A.[Resource] = 'Attendance'
