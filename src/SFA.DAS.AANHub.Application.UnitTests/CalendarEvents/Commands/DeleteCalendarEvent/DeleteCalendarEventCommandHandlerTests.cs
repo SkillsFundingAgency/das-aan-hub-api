138f7ae11c79bd17c4a7c90ac082cd762f9afcde
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-04-10 16:06:34 +0100

    CSP-1330 update email times to Local Time (#184)
    * CSP-1330 updated put calendar Event Command notification tokens to use corrected time
    
    * CSP-1330 extended tests to check notification tokens
    
    * CSP-1330 updated put and delete event and update attendance to use local time tokens
    
    * CSP-1330 removed unneeded comments

--- src/SFA.DAS.AANHub.Application.UnitTests/CalendarEvents/Commands/DeleteCalendarEvent/DeleteCalendarEventCommandHandlerTests.cs
+++ src/SFA.DAS.AANHub.Application.UnitTests/CalendarEvents/Commands/DeleteCalendarEvent/DeleteCalendarEventCommandHandlerTests.cs
@@ -3,10 +3,12 @@ using FluentAssertions.Execution;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.AANHub.Application.CalendarEvents.Commands.DeleteCalendarEvent;
+using SFA.DAS.AANHub.Application.Extensions;
 using SFA.DAS.AANHub.Domain.Entities;
 using SFA.DAS.AANHub.Domain.Interfaces;
 using SFA.DAS.AANHub.Domain.Interfaces.Repositories;
 using SFA.DAS.Testing.AutoFixture;
+using System.Text.Json;
 
 namespace SFA.DAS.AANHub.Application.UnitTests.CalendarEvents.Commands.DeleteCalendarEvent;
 
@@ -22,34 +24,58 @@ public class DeleteCalendarEventCommandHandlerTests
         [Frozen] Mock<IAuditWriteRepository> auditWriteRepository,
         [Frozen] DeleteCalendarEventCommandHandler sut,
         [Frozen] List<Member> members,
-        Guid calendarEventId,
+        CalendarEvent calendarEvent,
         Guid memberId)
     {
-        var command = new DeleteCalendarEventCommand(calendarEventId, memberId);
+        calendarEventsWriteRepository.Setup(x => x.GetCalendarEvent(It.IsAny<Guid>())).ReturnsAsync(calendarEvent);
+
+        var command = new DeleteCalendarEventCommand(calendarEvent.Id, memberId);
         var singleMemberId = members.First().Id;
 
         var attendances = new List<Attendance>
         {
             new()
             {
-                CalendarEventId = calendarEventId,
+                CalendarEventId = calendarEvent.Id,
                 MemberId = singleMemberId
             }
         };
 
-        attendancesWriteRepository.Setup(x => x.GetAttendancesByEventId(calendarEventId, It.IsAny<CancellationToken>()))
+        attendancesWriteRepository.Setup(x => x.GetAttendancesByEventId(calendarEvent.Id, It.IsAny<CancellationToken>()))
             .ReturnsAsync(attendances);
 
         membersReadRepository.Setup(x => x.GetMembers(It.IsAny<List<Guid>>(), It.IsAny<CancellationToken>())).ReturnsAsync(members);
 
         await sut.Handle(command, new CancellationToken());
 
+
+        var startDate = calendarEvent.StartDate.UtcToLocalTime();
+
+        var date = startDate.ToString("dd/MM/yyyy");
+        var time = startDate.ToString("HH:mm");
+
+        var tokens = new Dictionary<string, string>
+        {
+            { "contact", members.First().FullName },
+            { "eventname", calendarEvent.Title },
+            { "date", date },
+            { "time", time }
+        };
+
+        var expectedTokens = JsonSerializer.Serialize(tokens);
+
         using (new AssertionScope())
         {
-            calendarEventsWriteRepository.Verify(c => c.GetCalendarEvent(calendarEventId), Times.Once);
-            attendancesWriteRepository.Verify(a => a.GetAttendancesByEventId(calendarEventId, It.IsAny<CancellationToken>()), Times.Once);
+            calendarEventsWriteRepository.Verify(c => c.GetCalendarEvent(calendarEvent.Id), Times.Once);
+            attendancesWriteRepository.Verify(a => a.GetAttendancesByEventId(calendarEvent.Id, It.IsAny<CancellationToken>()), Times.Once);
             membersReadRepository.Verify(m => m.GetMembers(It.IsAny<List<Guid>>(), It.IsAny<CancellationToken>()), Times.Once);
-            notificationsWriteRepository.Verify(n => n.Create(It.IsAny<Notification>()), Times.Exactly(attendances.Count));
+            notificationsWriteRepository.Verify(n => n.Create(It.Is<Notification>(
+                q => q.MemberId == singleMemberId
+                     && q.TemplateName == Constants.NotificationTemplateNames.AANAdminEventCancel
+                     && q.CreatedBy == command.RequestedByMemberId
+                     && q.IsSystem == true
+                     && q.ReferenceId == calendarEvent.Id.ToString()
+                     && q.Tokens == expectedTokens)), Times.Once);
             auditWriteRepository.Verify(a => a.Create(It.IsAny<Audit>()), Times.Once);
             aanDataContext.Verify(a => a.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
         }
