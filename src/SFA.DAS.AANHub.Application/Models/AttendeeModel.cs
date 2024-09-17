f827f6b1f1b574e6b9347519a24a08a76aca4d78
Chris Foster <chrisfoster186@googlemail.com>
2024-09-17 15:18:11 +0100

    Captures event attendance cancellation date and exposes it through api

--- src/SFA.DAS.AANHub.Application/Models/AttendeeModel.cs
+++ src/SFA.DAS.AANHub.Application/Models/AttendeeModel.cs
@@ -3,7 +3,7 @@ using SFA.DAS.AANHub.Domain.Entities;
 
 namespace SFA.DAS.AANHub.Application.Models;
 
-public record AttendeeModel(Guid MemberId, UserType UserType, string MemberName, DateTime? AddedDate)
+public record AttendeeModel(Guid MemberId, UserType UserType, string MemberName, DateTime? AddedDate, DateTime? CancelledDate)
 {
-    public static implicit operator AttendeeModel(Attendance source) => new(source.MemberId, source.Member.UserType, source.Member.FullName, source.AddedDate);
+    public static implicit operator AttendeeModel(Attendance source) => new(source.MemberId, source.Member.UserType, source.Member.FullName, source.AddedDate, source.CancelledDate);
 }
